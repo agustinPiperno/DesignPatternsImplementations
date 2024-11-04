1d6b25e1bf06b57b6cf8fc97e7447b3b5714d8da
Agustin <agustin.piperno@gmail.com>
2024-11-03 22:15:17 -0300

    Implemented Observer pattern

--- Observer/Entities/ProgressionDisplay.cs
+++ Observer/Entities/ProgressionDisplay.cs
@@ -9,13 +9,24 @@ namespace ObserverPattern.Entities
 {
     internal class ProgressionDisplay : Display, Observer
     {
+        public decimal Weight { get; set; }
+        public uint Reps { get; set; }
+        private Subject exerciseData;
+
+        public ProgressionDisplay(Subject exerciseData)
+        {
+            this.exerciseData = exerciseData;
+            this.exerciseData.registerObserver(this);
+        }
+        public void update(decimal weight, uint reps)
+        {
+            this.Weight = weight;
+            this.Reps = reps;
+            this.display();
+        } 
         public void display()
         {
-
+            Console.WriteLine("Progression Display: " + this.Weight + " kg for " + this.Reps + " reps");
         }
-        public void update()
-        {
-
-        } 
     }
 }
--- Observer/Program.cs
+++ Observer/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using ObserverPattern.Entities;
+
+ExerciseData exerciseData = new ExerciseData();
+ProgressionDisplay progressionDisplay = new ProgressionDisplay(exerciseData);
+Estimated1RMDisplay estimated1RMDisplay = new Estimated1RMDisplay(exerciseData);
+
+exerciseData.setMeasurements(85, 5);
+exerciseData.removeObserver(progressionDisplay);
+exerciseData.setMeasurements(90, 4);
+exerciseData.registerObserver(progressionDisplay);
+exerciseData.setMeasurements(100, 2);
