7e09ce5f29276a7131f0df37ed8943bf53899125
Agustin <agustin.piperno@gmail.com>
2024-11-11 21:24:50 -0300

    Implemented Singleton pattern

---
+++ SIngletonPattern/Entities/Configuration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIngletonPattern.Entities
+{
+    internal class Configuration
+    {
+        private static Configuration? _configurationInstance;
+
+        private Configuration() { }
+        public static Configuration getInstance()
+        {
+            if(_configurationInstance == null)
+            {
+                _configurationInstance = new Configuration();
+            }
+            return _configurationInstance;
+        }
+    }
+}
